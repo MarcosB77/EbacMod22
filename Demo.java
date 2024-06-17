55cf5aaedfdfd1ce33efd25902fc3e8a284ce8ae
Marcos <M.G.Bl@example.com>
2024-06-17 13:01:14 -0400

    Atualiza a lógica para filtrar funcionários e corrigir possíveis erros de digitação no gênero

--- Demo.java
+++ Demo.java
@@ -2,27 +2,39 @@ package Empresa;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Demo {
     public static void main(String[] args) {
-        List<Funcionarios> lista = new Funcionarios().funcionariosLista();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite os dados dos funcionários no formato Nome-Gênero,Nome-Gênero,...:");
 
-        lista.stream().forEach(funcionarios -> System.out.println(funcionarios));
+        String input = scanner.nextLine();
+        String[] funcionariosArray = input.split(",");
+        List<Funcionarios> lista = new ArrayList<>();
+
+        for (String funcionarioData : funcionariosArray) {
+            String[] data = funcionarioData.split("-");
+            if (data.length == 2) {
+                String nome = data[0].trim();
+                String genero = data[1].trim();
+                lista.add(new Funcionarios(nome, genero));
+            } else {
+                System.out.println("Entrada inválida: " + funcionarioData);
+            }
+        }
+
+        System.out.println("Todos os Funcionários:");
+        lista.forEach(System.out::println);
 
         System.out.println("********");
-        System.out.println("********");
-
-
-        //lista mulheres
-        ArrayList<Funcionarios> list = lista.stream()
-                .filter(funcionarios -> funcionarios.getGenero().equals("Feminino"))
-                .collect(Collectors.toCollection(ArrayList::new));
-
         System.out.println("Mulheres Da Empresa:");
-        list.forEach(System.out::println);
 
+        List<Funcionarios> listaMulheres = lista.stream()
+                .filter(funcionarios -> funcionarios.getGenero().equalsIgnoreCase("Feminino"))
+                .collect(Collectors.toList());
 
-
+        listaMulheres.forEach(System.out::println);
     }
 }
--- Funcionarios.java
+++ Funcionarios.java
@@ -1,31 +1,14 @@
 package Empresa;
 
-import java.util.List;
-
 public class Funcionarios {
 
     private String nome;
-
     private String genero;
 
-    private String cargo;
 
-    private int idade;
-
-    public Funcionarios(String nome, String genero, String cargo, int idade) {
+    public Funcionarios(String nome, String genero) {
         this.nome = nome;
         this.genero = genero;
-        this.cargo = cargo;
-        this.idade = idade;
-
-    }
-
-    public Funcionarios() {
-        this.nome = nome;
-        this.genero = genero;
-        this.cargo = cargo;
-        this.idade = idade;
-
 
     }
 
@@ -45,41 +28,13 @@ public class Funcionarios {
         this.genero = genero;
     }
 
-    public String getCargo() {
-        return cargo;
-    }
 
-    public void setCargo(String cargo) {
-        this.cargo = cargo;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-    public List<Funcionarios> funcionariosLista(){
-
-
-    Funcionarios funcionarios1 = new Funcionarios("Lukas Santos", "Masculino","Admistração",44);
-    Funcionarios funcionarios2 = new Funcionarios("Isabela Gomes", "Feminino","TI",27);
-    Funcionarios funcionarios3 = new Funcionarios("Janaina Oliveira", "Feminino","Admistração",29);
-    Funcionarios funcionarios4 = new Funcionarios("Anna Costa","Feminino","Admistração",34);
-    return List.of(funcionarios1,funcionarios2,funcionarios3,funcionarios4);
-
-
-
-    }
 
     @Override
     public String toString() {
         return "Funcionarios{" +
                 "nome='" + nome + '\'' +
                 ", genero='" + genero + '\'' +
-                ", cargo='" + cargo + '\'' +
-                ", idade=" + idade +
                 '}';
     }
 }
